26cd04f8a8ab8f657cfd8707dfcac7c7a0b19a36
MurilloMoutinho <104108412+MurilloMoutinho@users.noreply.github.com>
2022-09-02 21:23:37 -0300

    Add files via upload

---
+++ LISTA1 EX2/LISTA1 EX2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA1_EX2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrdo soma1;
+            soma1 = new Quadrdo();
+
+            Console.Write("Digite a aresta do quadrado: ");
+            soma1.setaresta(int.Parse(Console.ReadLine()));
+
+            soma1.somar();
+
+            Console.WriteLine("A área do quadrado é: {0}", soma1.getResultado());
+        }
+    }
+}
---
+++ LISTA1 EX2/LISTA1 EX2/Quadrdo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA1_EX2
+{
+    internal class Quadrdo
+    {
+        private int aresta;
+        private int resultado;
+
+        public void setaresta(int n)
+        {
+            aresta = n;
+        }
+
+        public int getaresta()
+        {
+            return aresta;
+        }
+
+        public int getResultado()
+        {
+            return resultado;
+        }
+        public void somar()
+        {
+            resultado = aresta * aresta;
+        }
+    }
+
+}
